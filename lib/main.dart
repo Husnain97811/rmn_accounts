5a69ad454dca2d2aa9eafbbce0da13ec20b94e9b
Husnain Ahmed <83919975+Husnain97811@users.noreply.github.com>
2025-12-18 20:56:16 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // import 'dart:io';
-
+// THIS IS A COMPLETE PROJECT SO YOU CAN USE IT FOR ANYONE
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -167,3 +167,4 @@ class _AuthWrapperState extends State<AuthWrapper> {
     );
   }
 }
+
